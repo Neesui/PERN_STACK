746858af4c8eebaeee5ede26757c07816e28108e
Neesui <neeshachy2020@gmail.com>
2025-05-17 21:46:39 +0545

    BIbash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Tablelist from './components/Tablelist'
+import Modelform from './components/Modelform'
 
 const App = () => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const [modalMode, setmodalMode] = React.useState('add');
+
+  const handleOpen = (mode) =>{
+    setIsOpen(true)
+  }
+  const handleSubmit = () =>{
+    if(mode === 'add'){
+      console.log('modal mode add')
+    } else{
+      console.log('modal mode edit')
+    }
+  }
   return (
     <div className=''>
-      <button className="btn btn-neutral	">Defaul</button>
+      <Navbar  onOpen={()=> handleOpen('add')}/>
+      <Tablelist />
+      <Modelform 
+      isOpen={isOpen} onSubmit={handleSubmit}
+      onClose ={() => setIsOpen(false)} />
     </div>
   )
 }
